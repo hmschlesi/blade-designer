8b6072430fb46b20de4927f5f7aeb0bab59e5d71
tstrut <107057437+tstrut@users.noreply.github.com>
2022-06-13 00:31:59 +0200

    Add files via upload
    Working Matlab code to open a SWX model an run the attached macro

---
+++ Matlab_SWX.m
@@ -0,0 +1,4 @@
+swApp = actxserver('SldWorks.Application');
+set(swApp, 'Visible', true);
+O1 =invoke(swApp,'OpenDoc','C:\Users\Tom\Desktop\WEA II\Teil6.SLDPRT',1);
+S1=invoke(swApp ,'RunAttachedMacro','Makro-Profil-laden.swp', 'Makro_Profil_laden1', 'main');
