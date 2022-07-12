f69f57d60a3f2d18e6a18750eb4a4649a1775cb7
hmschlesi <hmschlesi@googlemail.com>
2022-07-12 14:16:12 +0200

    foil_importpol created and updated foil_import()
    foil-importpol imports airfoil tool database foils with given polar data.
    foil_import() now imports polar data and new foils with xfoil.

--- foil_evaluate.m
+++ foil_evaluate.m
@@ -1,4 +1,6 @@
 function [pol,coords]=foil_evaluate(name, file_path, file_name)
+%%evaluateds a given profile with xfoil
+%might have issues with convergence, so its slighly unstable
 
  if regexp(name,'naca([0-9]{4})$') == 1 | regexp(name,'NACA([0-9]{4})$') == 1 
         A=readmatrix(file_path,'NumHeaderLines',1);
@@ -11,8 +13,8 @@ function [pol,coords]=foil_evaluate(name, file_path, file_name)
  AoA=linspace(0,20,21);
     if regexp(name,'naca([0-9]{4})$') == 1  | regexp(name,'NACA([0-9]{4})$') == 1 
         %%prüft ob ein 4-digit NACA Profil eingegeben wurde z.B. 'NACA0012'
-        [pol,coords]=xfoil(name,AoA,1e5,0,join([name,' oper iter 160']));
+        [pol,coords]=xfoil(name,AoA,1e5,0,join([name,' oper iter 300']));
     else
-        [pol,coords]=xfoil(file_name,AoA,1e5,0,join([name,' oper iter 160']));
+        [pol,coords]=xfoil(file_name,AoA,1e5,0,join([name,' oper iter 300']));
     end
     coords=A;
--- foil_import.m
+++ foil_import.m
@@ -2,10 +2,73 @@
 %% imports coords and runs them trough xfoil to calculate coefficents
 %% saves the coords and coefficents in the folder 'foils'
 function foil_import()
-listing=dir('foils_4_import');
 foil_db_path='foils_imported.dat'
-l=length(listing);
 
+
+%%import foils with given polar data
+
+listing=dir('foils_4_import_pol');
+l=length(listing);
+for i=3:l
+    file_folder=listing(i).folder;
+    file_name=listing(i).name;
+    name=erase(file_name, [".txt",".dat","_pol.dat"])
+    file_path=join(['foils_4_importpol\',file_name]);
+     if isfile(foil_db_path)
+        
+        foil_db=readtable(foil_db_path);
+        Index =find(strcmp(name,foil_db.foil_name));
+        if isempty(Index)
+           
+             [pol,A]=foil_importpol(name);
+
+            epsilon=pol.CL./pol.CD;
+            [M,I]=max(epsilon);
+            AoA_eps(1,1)=pol.alpha(I);
+            AoA_eps(1,2)=pol.CL(I);
+            AoA_eps(1,3)=pol.CD(I);
+            [M,I]=max(pol.CL);
+            AoA_lift(1,1)=pol.alpha(I);
+            AoA_lift(1,2)=pol.CL(I);
+            AoA_lift(1,3)=pol.CD(I);
+
+            T=table(string(name), AoA_eps(1,1),AoA_eps(1,2),AoA_eps(1,3),AoA_lift(1,1),AoA_lift(1,2),AoA_lift(1,3));
+            T.Properties.VariableNames={'foil_name','AoA_eps','CL_eps','CD_eps','AoA_lift','CL_lift','CD_lift'};
+            foil_db=[foil_db;T];
+            writetable(foil_db,foil_db_path);
+            disp(join(['Profil ',name,' wurde importiert']))
+        else
+            disp('foil already imported')
+        end
+        
+    else
+        %% checks if foil_db exists
+        [pol,A]=foil_importpol(name);
+
+        epsilon=pol.CL./pol.CD;
+        [M,I]=max(epsilon);
+        AoA_eps(1,1)=pol.alpha(I);
+        AoA_eps(1,2)=pol.CL(I);
+        AoA_eps(1,3)=pol.CD(I);
+        [M,I]=max(pol.CL);
+        AoA_lift(1,1)=pol.alpha(I);
+        AoA_lift(1,2)=pol.CL(I);
+        AoA_lift(1,3)=pol.CD(I);
+        
+        T=table(string(name), AoA_eps(1,1),AoA_eps(1,2),AoA_eps(1,3),AoA_lift(1,1),AoA_lift(1,2),AoA_lift(1,3));
+        T.Properties.VariableNames={'foil_name','AoA_eps','CL_eps','CD_eps','AoA_lift','CL_lift','CD_lift'};    
+
+        writetable(T,foil_db_path);
+
+    
+        foil_save(pol,A,name);
+    end
+end
+
+ 
+%%import foils with xfoil
+listing=dir('foils_4_import');
+l=length(listing);
 for i=3:l
     file_folder=listing(i).folder;
     file_name=listing(i).name;
@@ -62,8 +125,6 @@ for i=3:l
         foil_save(pol,A,name);
     end
 end
-for i=3:l
-    listing_new(i-2).name=erase(listing(i).name, [".txt",".dat"]);
 end
 %T_temp=struct2table(listing_new);
 %T=table(T_temp.name, AoA_eps(:,1),AoA_eps(:,2),AoA_eps(:,3),AoA_lift(:,1),AoA_lift(:,2),AoA_lift(:,3));
