a0e718aa0e21fbff7026bdcd4975b63f909c62ea
tstrut <107057437+tstrut@users.noreply.github.com>
2022-07-15 22:13:16 +0200

    identical with Matlab_call in Tool folder

--- Matlab_SWX.m
+++ Matlab_SWX.m
@@ -1,5 +1,7 @@
+currentFile = mfilename( 'fullpath' );
+[pathstr,~,~] = fileparts( currentFile );
 swApp = actxserver('SldWorks.Application');
 set(swApp, 'Visible', true);
-%% change to universal folder format!
-O1 =invoke(swApp,'OpenDoc','C:\Users\Tom\Desktop\WEA II\Teil6.SLDPRT',1);
-S1=invoke(swApp ,'RunAttachedMacro','Makro-Profil-laden.swp', 'Makro_Profil_laden1', 'main');
+O1 = invoke(swApp,'OpenDoc',append(pathstr,'\Teil6.SLDPRT'),1);
+M1 = invoke(swApp, 'SetCurrentWorkingDirectory', pathstr);
+S1 = invoke(swApp ,'RunAttachedMacro','Makro-Profil-laden.swp', 'Makro_Profil_laden1', 'main');
