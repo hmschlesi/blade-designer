1291fd506f6958737fe6361f2a7280341edbc3d9
tstrut <107057437+tstrut@users.noreply.github.com>
2022-07-15 22:08:59 +0200

    Delete Tool directory

--- Tool/Matlab_call.m
+++
@@ -1,7 +0,0 @@
-currentFile = mfilename( 'fullpath' );
-[pathstr,~,~] = fileparts( currentFile );
-swApp = actxserver('SldWorks.Application');
-set(swApp, 'Visible', true);
-O1 = invoke(swApp,'OpenDoc',append(pathstr,'\Teil6.SLDPRT'),1);
-M1 = invoke(swApp, 'SetCurrentWorkingDirectory', pathstr);
-S1 = invoke(swApp ,'RunAttachedMacro','Makro-Profil-laden.swp', 'Makro_Profil_laden1', 'main');
