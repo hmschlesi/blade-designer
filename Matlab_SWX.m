cf7bfaee0122a2f9399cd4906ea0839b1dc3b4cd
tstrut <107057437+tstrut@users.noreply.github.com>
2022-07-16 00:21:32 +0200

    calls macro directly, ditched part6

--- Matlab_SWX.m
+++ Matlab_SWX.m
@@ -2,6 +2,5 @@ currentFile = mfilename( 'fullpath' );
 [pathstr,~,~] = fileparts( currentFile );
 swApp = actxserver('SldWorks.Application');
 set(swApp, 'Visible', true);
-O1 = invoke(swApp,'OpenDoc',append(pathstr,'\Teil6.SLDPRT'),1);
 M1 = invoke(swApp, 'SetCurrentWorkingDirectory', pathstr);
-S1 = invoke(swApp ,'RunAttachedMacro','Makro-Profil-laden.swp', 'Makro_Profil_laden1', 'main');
+S1 = invoke(swApp, 'RunMacro', strcat(pathstr, '\Makro-Profil-laden.swp'), 'Makro_Profil_laden1', 'main');
