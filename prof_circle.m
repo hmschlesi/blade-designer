f6420ee61df76cb017e36eb5ce60da00ebf4eaf8
vicpe99 <64752811+vicpe99@users.noreply.github.com>
2022-07-17 22:22:33 +0200

    Update prof_circle.m

--- prof_circle.m
+++ prof_circle.m
@@ -1,7 +1,7 @@
 function profile=prof_circle(d,r,n)
 phi=linspace(0,2*pi,n);
 %profile.x=d./2.*cos(phi)+(0.5.*d);
-profile.x=d./2.*cos(phi)
+profile.x=d./2.*cos(phi);
 profile.y=d./2.*sin(phi);
 profile.z=ones(1,n)*r;
 profile.Cd=0.43;
@@ -9,4 +9,4 @@ profile.Cl=0;
 profile.camber=d;
 profile.alpha_bau=0;
 profile.r=r;
-profile.name=join(['circle ',num2str(d),' m'])
+profile.name=join(['circle ',num2str(d),' m']);
